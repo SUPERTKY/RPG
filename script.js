68668c9f344d6d6ef3e00acf118f02378dc29753
SUPERTKY <purple_chips@outlook.jp>
2025-04-18 18:27:44 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const bgm = document.getElementById("bgm");
-
-// ユーザー操作がないと音楽がブロックされる場合があるので、ボタンで開始
-document.getElementById("start-btn").addEventListener("click", () => {
-  bgm.play();
-  alert("ゲーム開始！（次の画面に進む処理をここに書きます）");
+// ブラウザによっては自動再生が制限されているため、クリックで再生
+window.addEventListener("click", () => {
+  const bgm = document.getElementById("bgm");
+  if (bgm.paused) {
+    bgm.play();
+  }
 });
