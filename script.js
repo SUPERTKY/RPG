63f99aa9bda920cf681ceb57352e898553386c83
SUPERTKY <purple_chips@outlook.jp>
2025-04-21 13:35:57 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ window.addEventListener("load", () => {
   const startButton = document.getElementById("start-button");
   const playButton = document.getElementById("play-button");
     const playSound = document.getElementById("play-sound"); // ← 効果音
+   const fadeOverlay = document.getElementById("fade-overlay");
   //playSound.volume = 0.3;
   const storedName = localStorage.getItem("playerName");
 
